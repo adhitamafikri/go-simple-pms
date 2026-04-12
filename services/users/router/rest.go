7e3a56da91cea32be19f9d9475eaa84b5e50d63b
Adhitama Fikri <adhitamafikri.dev@gmail.com>
2026-04-12 22:49:46 +0700

    fix(users): bump to go version 1.26, rearrange router, env example with value

--- services/users/router/rest.go
+++ services/users/router/rest.go
@@ -8,14 +8,13 @@ import (
 
 func LoadRouter() *gin.Engine {
 	router := gin.Default()
-
-	group := router.Group("users")
-	group.GET("ping", func(ctx *gin.Context) {
+	router.GET("ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Pong",
 		})
 	})
 
+	group := router.Group("users")
 	group.GET("", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Welcome to users service",
